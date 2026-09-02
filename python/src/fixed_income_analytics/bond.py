55cecb86dfc1520ebe45dd12441d238edc63ca0a
BrianLee11 <brian.lee.great@gmail.com>
2026-09-02 14:36:51 -0400

    Add bond notebooks and configure Python package

--- python/src/fixed_income_analytics/bond.py
+++ python/src/fixed_income_analytics/bond.py
@@ -1,14 +1,28 @@
 import datetime
+""" TODO: create a bond class consisted of
+1. face value (e.g. $100)
+2. coupon rate (e.g. 4%)
+3. maturity date (e.g. 2036-09-02)
+4. payment frequency (e.g. annual, semiannual, quarterly, monthly)
+"""
 
 class Bond:
     def __init__(self, 
                  face_value: float, 
                  coupon_rate: float, 
                  maturity: datetime.date,
-                 payment_frequency: int # 1 = annual, 2 = semiannual, 4 = quarterly, 12 = monthly
+                 payment_frequency: int = 2 # 1 = annual, 2 = semiannual, 4 = quarterly, 12 = monthly
                  ):
         self.face_value: float = face_value
         self.coupon_rate: float = coupon_rate
         self.maturity: datetime.date = maturity
-        self.payment_frequency: int = 2
+        self.payment_frequency: int = payment_frequency
+
+""" TODO: show an example of initializing a bond
+bond1 = Bond(100, 0.04, datetime.date(2036-09-02), 2)
+print(bond1.face_value)  # 100
+print(bond1.coupon_rate) # 0.04
+print(bond1.maturity)    # 2036-09-02
+print(bond1.payment_frequency)  # 2
+"""
         
